0a6e0c8012e426b064540bc76caada05996be8c6
Karl Stenerud <kstenerud@gmail.com>
2018-06-01 10:09:17 -0700

    Decoder framework code

---
+++ reference-implementation/include/cbe/decoder.h
@@ -0,0 +1,51 @@
+#ifndef dbe_decoder_H
+#define dbe_decoder_H
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+	void (*on_empty)(void);
+	void (*on_bool)(bool value);
+	void (*on_date)(int year, int month, int day, int hour, int minute, int second);
+	void (*on_timestamp_ms)(int year, int month, int day, int hour, int minute, int second, int ms);
+	void (*on_timestamp_ns)(int year, int month, int day, int hour, int minute, int second, int ns);
+	void (*on_end_container)(void);
+	void (*on_list_start)(void);
+	void (*on_map_start)(void);
+	void (*on_string)(int8_t* start, int8_t* end);
+	void (*on_bytes)(uint8_t* start, uint8_t* end);
+	void (*on_int_8)(int8_t value);
+	void (*on_int_16)(int16_t value);
+	void (*on_int_32)(int32_t value);
+	void (*on_int_64)(int64_t value);
+	void (*on_int_128)(__int128 value);
+	void (*on_float_32)(float value);
+	void (*on_float_64)(double value);
+	void (*on_float_128)(long double value);
+	// void (*on_decimal_64_dpd)(??? value);
+	// void (*on_decimal_128_dpd)(??? value);
+	// void (*on_decimal_64_bid)(??? value);
+	// void (*on_decimal_128_bid)(??? value);
+	void (*on_array_int_16)(int16_t* start, int16_t* end);
+	void (*on_array_int_32)(int32_t* start, int32_t* end);
+	void (*on_array_int_64)(int64_t* start, int64_t* end);
+	void (*on_array_int_128)(__int128* start, __int128* end);
+	void (*on_array_float_32)(float* start, float* end);
+	void (*on_array_float_64)(double* start, double* end);
+	void (*on_array_float_128)(long double* start, long double* end);
+	// Array date, timestamps
+	// Array bool
+} cbe_decode_callbacks;
+
+
+void cbe_decode(cbe_decode_callbacks* callbacks, const uint8_t* const data_start, const uint8_t* const data_end);
+
+
+#ifdef __cplusplus 
+}
+#endif
+#endif // dbe_decoder_H
---
+++ reference-implementation/src/decoder.c
@@ -0,0 +1,188 @@
+#include <stdlib.h>
+#include "cbe/decoder.h"
+#include "cbe_internal.h"
+
+void cbe_decode(cbe_decode_callbacks* callbacks, const uint8_t* const data_start, const uint8_t* const data_end)
+{
+	for(const uint8_t* ptr = data_start; ptr < data_end;)
+	{
+		type_field type = *ptr++;
+		switch(type)
+		{
+			case TYPE_EMPTY:
+			callbacks->on_empty();
+		        break;
+			case TYPE_FALSE:
+				callbacks->on_bool(false);
+		        break;
+			case TYPE_TRUE:
+				callbacks->on_bool(true);
+		        break;
+			case TYPE_DATE:
+			{
+				callbacks->on_date(1,1,1,1,1,1);
+		        break;
+		    }
+			case TYPE_TIMESTAMP_MS:
+			{
+				callbacks->on_timestamp_ms(1,1,1,1,1,1,1);
+		        break;
+		    }
+			case TYPE_TIMESTAMP_NS:
+			{
+				callbacks->on_timestamp_ns(1,1,1,1,1,1,1);
+		        break;
+		    }
+			case TYPE_END_CONTAINER:
+				callbacks->on_end_container();
+		        break;
+			case TYPE_LIST:
+				callbacks->on_list_start();
+		        break;
+			case TYPE_MAP:
+				callbacks->on_map_start();
+		        break;
+			case TYPE_ARRAY:
+				// TODO
+		        break;
+			case TYPE_STRING:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_0:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_1:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_2:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_3:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_4:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_5:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_6:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_7:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_8:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_9:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_10:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_11:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_12:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_13:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_14:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_STRING_15:
+				callbacks->on_string(NULL, NULL);
+		        break;
+			case TYPE_BYTES_0:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_1:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_2:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_3:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_4:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_5:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_6:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_7:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_8:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_9:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_10:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_11:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_12:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_13:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_14:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES_15:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_BYTES:
+				callbacks->on_bytes(NULL, NULL);
+		        break;
+			case TYPE_INT_16:
+				callbacks->on_int_16(0);
+		        break;
+			case TYPE_INT_32:
+				callbacks->on_int_32(0);
+		        break;
+			case TYPE_INT_64:
+				callbacks->on_int_64(0);
+		        break;
+			case TYPE_INT_128:
+				callbacks->on_int_128(0);
+		        break;
+			case TYPE_FLOAT_32:
+				callbacks->on_float_32(0);
+		        break;
+			case TYPE_FLOAT_64:
+				callbacks->on_float_64(0);
+		        break;
+			case TYPE_FLOAT_128:
+				callbacks->on_float_128(0);
+		        break;
+			case TYPE_DECIMAL_BID_64:
+				// callbacks->on_decimal_64_bid(0);
+		        break;
+			case TYPE_DECIMAL_BID_128:
+				// callbacks->on_decimal_128_bid(0);
+		        break;
+			case TYPE_DECIMAL_DPD_64:
+				// callbacks->on_decimal_64_dpd(0);
+		        break;
+			case TYPE_DECIMAL_DPD_128:
+				// callbacks->on_decimal_128_dpd(0);
+		        break;
+		    default:
+		    	callbacks->on_int_8(type);
+		    	break;
+		}
+	}
+}
