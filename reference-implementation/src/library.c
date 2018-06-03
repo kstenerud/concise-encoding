846e3241af54dad42e242eff41a90db87ff6f0e5
Karl Stenerud <kstenerud@gmail.com>
2018-06-02 17:14:31 -0700

    Add version API

---
+++ reference-implementation/src/library.c
@@ -0,0 +1,7 @@
+#include "cbe/cbe.h"
+#include "cbe_version.h"
+
+const char* cbe_version()
+{
+	return CBE_VERSION;
+}
--- reference-implementation/test/src/main.cpp
+++ reference-implementation/test/src/main.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <cbe/cbe.h>
 
 int main(int argc, char **argv)
 {
