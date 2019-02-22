503909f2d13c3fb30555c751516f45db1cc05f89
Karl Stenerud <kstenerud@gmail.com>
2019-02-22 19:14:56 +0100

    Cover more in validation

--- reference-implementation/cbe/src/library.c
+++ reference-implementation/cbe/src/library.c
@@ -81,22 +81,19 @@ static bool validate_utf8(utf8_context* context, uint8_t ch)
 
 static bool validate_comment(int ch)
 {
-    switch(ch)
+    if(ch >= 0x00 && ch <= 0x1f && ch != 0x09)
     {
-        case 0x00: case 0x01: case 0x02: case 0x03:
-        case 0x04: case 0x05: case 0x06: case 0x07:
-        case 0x08: /*case 0x09:*/ case 0x0a: case 0x0b:
-        case 0x0c: case 0x0d: case 0x0e: case 0x0f:
-        case 0x10: case 0x11: case 0x12: case 0x13:
-        case 0x14: case 0x15: case 0x16: case 0x17:
-        case 0x18: case 0x19: case 0x1a: case 0x1b:
-        case 0x1c: case 0x1d: case 0x1e: case 0x1f:
-        case 0x7f:
-        case 0x85:
-        case 0x2028:
-        case 0x2029:
-            return false;
+        return false;
     }
+    if(ch >= 0x7f && ch <= 0x9f)
+    {
+        return false;
+    }
+    if(ch == 0x2028 || ch == 0x2029)
+    {
+        return false;
+    }
+
     return true;
 }
 
