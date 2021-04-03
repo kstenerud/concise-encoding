e28eef8e2a2c193569776b616120ed4a1b2c043b
Karl Stenerud <kstenerud@gmail.com>
2021-04-03 19:02:19 +0200

    Update domation buttons and copy

--- minify.sh
+++ minify.sh
@@ -5,31 +5,21 @@ set -eux
 # sudo npm install -g html-minifier
 # sudo apt install yui-compressor
 
-html-minifier --collapse-whitespace \
-			  --remove-comments \
-			  --remove-optional-tags \
-			  --remove-redundant-attributes \
-			  --remove-script-type-attributes \
-			  --remove-tag-whitespace \
-			  --use-short-doctype \
-			  src/index.html >index.html
+minify() {
+	local html=$1
+	html-minifier --collapse-whitespace \
+				  --remove-comments \
+				  --remove-optional-tags \
+				  --remove-redundant-attributes \
+				  --remove-script-type-attributes \
+				  --remove-tag-whitespace \
+				  --use-short-doctype \
+				  src/$html >$html
+}
 
-html-minifier --collapse-whitespace \
-			  --remove-comments \
-			  --remove-optional-tags \
-			  --remove-redundant-attributes \
-			  --remove-script-type-attributes \
-			  --remove-tag-whitespace \
-			  --use-short-doctype \
-			  src/resources.html >resources.html
-
-html-minifier --collapse-whitespace \
-			  --remove-comments \
-			  --remove-optional-tags \
-			  --remove-redundant-attributes \
-			  --remove-script-type-attributes \
-			  --remove-tag-whitespace \
-			  --use-short-doctype \
-			  src/support-ce.html >support-ce.html
+minify index.html
+minify resources.html
+minify support-ce.html
+minify thankyou.html
 
 yui-compressor src/main.css >main.css
