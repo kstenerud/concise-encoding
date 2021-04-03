58449ecd9c060afb5b72cb31d969127180bf4211
Karl Stenerud <kstenerud@gmail.com>
2021-04-03 18:30:16 +0200

    Add donate button and outline 'support CE' page

--- minify.sh
+++ minify.sh
@@ -23,4 +23,13 @@ html-minifier --collapse-whitespace \
 			  --use-short-doctype \
 			  src/resources.html >resources.html
 
+html-minifier --collapse-whitespace \
+			  --remove-comments \
+			  --remove-optional-tags \
+			  --remove-redundant-attributes \
+			  --remove-script-type-attributes \
+			  --remove-tag-whitespace \
+			  --use-short-doctype \
+			  src/support-ce.html >support-ce.html
+
 yui-compressor src/main.css >main.css
